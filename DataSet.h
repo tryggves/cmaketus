ec4932d179102d9a9cbdaac886b99375d2c10f07
tryggve <tryggve.sorensen@gmail.com>
2019-03-02 11:44:06 +0100

    Implementation of persist done and compiles.

--- DataSet.h
+++ DataSet.h
@@ -10,15 +10,19 @@
 #define CMAKETUT_DATASET_H
 
 #include <string>
+#include <cstdio>                   // C stdio.h standard IO library
 
 class DataSet {
 private:
+    FILE *dsFile;                       // File stream handler reference
+    const std::string persistFileName = "dataset.txt";  // File where to store the data set.
     std::string dataSetBuffer;         // Dataset buffer - simple example a string array
     // char dataSetBuffer[256];        // Dataset buffer - simple example a string array
 
 public:
     DataSet();                      // Default constructor
     void printDataset();            // Create a nice output of the dataset.
+    int saveDataset();              // Persist dataset to file
 };
 
 
