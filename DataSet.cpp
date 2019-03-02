f9a51be8c5a2e8606aadb78c4ce35b4b0fc49b0e
tryggve <tryggve.sorensen@gmail.com>
2019-03-02 08:20:25 +0100

    Adding dataset class.

---
+++ DataSet.cpp
@@ -0,0 +1,5 @@
+//
+// Created by tryggve on 3/2/19.
+//
+
+#include "DataSet.h"
---
+++ DataSet.h
@@ -0,0 +1,18 @@
+//
+// Created by tryggve on 3/2/19.
+//
+// Simple file writer class representing a dataset.
+// Functionality
+//
+//
+
+#ifndef CMAKETUT_FILEWRITER_H
+#define CMAKETUT_FILEWRITER_H
+
+
+class DataSet {
+
+};
+
+
+#endif //CMAKETUT_FILEWRITER_H
