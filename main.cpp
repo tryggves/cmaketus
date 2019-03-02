fb675451a996352c8fc174b5424f669b9cbb8421
tryggve <tryggve.sorensen@gmail.com>
2019-03-02 08:10:42 +0100

    Some more comments and fixing a bit on main function.

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ int main(int argc, char *argv[]) {
     std::cout << "New mouse is: " << mouse.getBrand() << " with serial number " << mouse.getSerial() << std::endl;
 
     if (argc < 2) {
-        std::cout << "Version " << Tutorial_VERSION_MAJOR << "." << Tutorial_VERSION_MINOR << std::endl;
+        std::cout << "Tutorial program Version " << Tutorial_VERSION_MAJOR << "." << Tutorial_VERSION_MINOR << std::endl;
         std::cout << "Usage: " << argv[0] << " <number>" << std::endl;
 
         return 1;
