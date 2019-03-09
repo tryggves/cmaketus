61d520517af6d78d2ee973616d27f5b3c6e1fee2
tryggve <tryggve.sorensen@gmail.com>
2019-03-09 17:11:22 +0100

    Implementation of DataSet::readDataSet().

--- DataSet.cpp
+++ DataSet.cpp
@@ -7,7 +7,15 @@
 
 // Default constructor inserts so
 DataSet::DataSet() {
-    dataSetBuffer = "Hello world!";
+    int numCharsInDataset;
+
+    numCharsInDataset = readDataset();
+    if (numCharsInDataset < 0) {
+        // Either file not exist or fatal error in method
+        // Just set the default value
+        fprintf(stderr, "Setting default value for dataset.\n");
+        dataSetBuffer = "Hello world!";
+    }
 }
 
 
@@ -42,4 +50,44 @@ int DataSet::saveDataset() {
 
     return numCharsWritten;
 
+}
+
+// Read dataset from file.
+int DataSet::readDataset() {
+    // char * cstring = new char [256];        // Assume this is the max length of data set
+    char * buffer;                              // Temporary buffer for dataset
+    long lSize;                                 // Size of dataset
+    int numCharsRead = 0;                       // Number of characters read
+
+    // Open the persist file
+    dsFile = fopen(persistFileName.c_str(), "r");
+
+    if (dsFile == nullptr) {
+        fprintf(stderr, "Error opening file: %s. Error code is: %i\n", persistFileName.c_str(), errno);
+        return -1;
+    }
+
+    // Get the file size
+    fseek (dsFile, 0, SEEK_END);
+    lSize = ftell(dsFile);
+    rewind(dsFile);
+
+    // Allocate temporary memory to contain file content
+    buffer = (char *) malloc(sizeof(char)*lSize);
+    if (buffer == nullptr) {
+        fprintf(stderr, "Memory error\n");
+        return (-1);
+    }
+
+    // Read the dataset from the file
+    numCharsRead = fread(buffer, 1, lSize, dsFile);
+    if (numCharsRead != lSize) {
+        fprintf(stderr, "Reading dataset error!\n");
+        return (-1);
+    }
+
+    // Create the string member holding the new string from the buffer.
+    dataSetBuffer.assign(buffer);
+
+    return numCharsRead;
 }
