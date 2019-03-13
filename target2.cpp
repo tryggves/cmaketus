a764b914b1f7c1974c08f68c89f5c73f71936235
Tryggve Sorensen <tryggve.sorensen@me.com>
2019-03-13 21:41:45 +0100

    New target executable added to project

---
+++ target2.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Tryggve Sørensen on 2019-03-13.
+//
+
+// Demo making another executable and add it to the CMakeLists.txt file
+
+#include <iostream>
+
+using namespace std;
+
+void main(int argc, char **argv) {
+    cout << "Hello world" << endl;
+}
